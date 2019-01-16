0bd97fdfb2fba9f334c18de46b94ff08afc3ca2d
DanielKwanUkYoo <kwanukyoo0722@gmail.com>
2019-01-16 03:22:08 +0000

    completed ex7

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -10,14 +10,7 @@ puts "----------"
 
 # Your code goes here ...
 
-class Employee < ActiveRecord::Base
-    belongs_to :store
-end
-
-
-@store1.employees.create(first_name: "Daniel", last_name: "Yoo", hourly_rate: 40)
-@store1.employees.create(first_name: "Michael", last_name: "smthing", hourly_rate: 50)
-@store2.employees.create(first_name: "Jenny", last_name: "Nam", hourly_rate: 60)
+@store1.employees.create(first_name: "Daniel", last_name: "Yoo", hourly_rate: 20)
+@store1.employees.create(first_name: "Michael", last_name: "chung", hourly_rate: 50)
+@store2.employees.create(first_name: "nil", last_name: "Nam", hourly_rate: 60)
 @store2.employees.create(first_name: "Nick", last_name: "Zac", hourly_rate: 20)
-# @store3.employees.create(first_name: "chris", last_name: "last", hourly_rate: 30)
-# @store3.employees.create(first_name: "tttt", last_name: "tttt", hourly_rate: 90)
