1b0b530048f6f2878ccc8f69b2350cae77dab387
DanielKwanUkYoo <kwanukyoo0722@gmail.com>
2019-01-16 01:31:00 +0000

    completed ex5

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,18 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+total_rev = Store.all.sum(:annual_revenue)
+puts "total rev of all the stores: #{total_rev}"
+avg_rev = total_rev / Store.count
+puts "average rev #{avg_rev}"
+rev_over_1m = Store.all.where('annual_revenue > ?', 1000000).count
+puts "stores that makes over 1m: #{rev_over_1m}"
+
+
+
+# sum = 0
+
+# Store.all.each do |each|
+#     sum += each.annual_revenue
+# end
+# puts sum
